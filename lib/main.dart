2af2fd5d3fb493f1406c41fea465fff5f8a5ce84
mazab99 <mazab15499@gmail.com>
2023-05-11 10:40:42 +0300

    add egypt flag

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
       title: 'Mahmoud Azab',
       debugShowCheckedModeBanner: false,
